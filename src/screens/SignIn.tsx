8fe4a7271fb773f8582f6664e1c5fef8d5ea845e
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-05-08 23:09:57 -0300

    feat: :sparkles: add screen signin and signup in project

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,50 @@
+
+import { VStack,Image, Text, Center, Heading, ScrollView } from 'native-base';
+import React from 'react';
+import BackgroundImage from '@assets/background.png';
+import LogoSvg from '@assets/logo.svg';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+export function SignIn() {
+  return (
+    <ScrollView contentContainerStyle={{flexGrow:1}} showsVerticalScrollIndicator={false}>
+    <VStack flex={1} bg="gray.700" px={10} pb={16}>
+    <Image 
+     source={BackgroundImage} 
+     alt='Pessoas treinando'
+     resizeMode='contain'
+     position={'absolute'}
+     />
+     <Center my={24}>
+
+     <LogoSvg/>
+     <Text color={'gray.100'} fontSize={'sm'}>Treine sua mente e o seu corpo</Text>
+     </Center>
+     <Center>
+     <Heading color={'gray.100'} fontSize={'xl'} mb={6} fontFamily={'heading'}>
+      Acesso sua conta
+      </Heading>
+      <Input 
+      placeholder='E-mail'
+      keyboardType='email-address'
+      autoCapitalize='none'
+
+    />
+    <Input 
+      placeholder='Senha'
+      secureTextEntry
+
+    />
+    <Button title='Acessar'/>
+
+     </Center>
+<Center pt={24}>
+     <Text  color={'gray.100'} fontSize={'sm' } mb={3} fontFamily={'body'} >Ainda não têm acesso?</Text>
+    <Button 
+    title='Criar conta'
+     variant={'outline'}/>
+    </Center>
+    </VStack>
+    </ScrollView>
+  );
+}
