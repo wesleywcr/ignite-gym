ace725634a9650e68d09807cc756d602b215f2fc
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-05-22 23:33:00 -0300

    feat: :sparkles: add seleted image in smartphone

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from 'expo-file-system';
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { Center, Heading, ScrollView, Skeleton, Text, VStack, useToast } from "native-base";
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
 
@@ -10,8 +12,42 @@ const PHONE_SIZES = 33
 
 export function Profile(){
   const [photoIsLoading, setPhotoIsloading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://github.com/wesleywcr.png')
+  const Toast = useToast();
 
+  async function handleUserPhotoSelect() {
+    try{
+      setPhotoIsloading(true);
 
+      const photoSelected =  await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality:1,
+        aspect:[4,4],
+        allowsEditing:true,
+      });
+      if(photoSelected.canceled){
+        return;
+      }
+      if (!photoSelected.canceled) {
+        const photoInfo = FileSystem.getInfoAsync(photoSelected.assets[0].uri)
+      console.log('if',(await photoInfo).uri)
+      //    if(photoInfo && (photoInfo.size/1024/1024)>5){
+      //   return  Toast.show({
+      //     title:'Essa imagem é muito grande. Escolha uma até 5mb.',
+      //     placement:'top',
+      //     bgColor:'red.500'
+      //   })
+      //  }
+        setUserPhoto(photoSelected.assets[0].uri)
+      }
+    }catch(error:any){
+      console.error('Erro')
+    }finally{
+      setPhotoIsloading(false);
+    }
+  
+   
+  }
 
   return(
     <VStack flex={1}>
@@ -29,13 +65,13 @@ export function Profile(){
           />
           :
           <UserPhoto
-       source={{uri:'https://github.com/wesleywcr.png'}} 
+       source={{uri:userPhoto}} 
        alt="Foto do usuário"
        size={PHONE_SIZES}
        />
         }
       
-       <TouchableOpacity>
+       <TouchableOpacity onPress={ handleUserPhotoSelect}>
         <Text color={'green.500'} 
         fontWeight={'bold'} 
         fontSize={'md'} 
