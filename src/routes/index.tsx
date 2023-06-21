f1eaeec6f07738b988afd47ae857e2412754bef3
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-06-20 22:53:57 -0300

    feat: add auth in app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,23 +2,25 @@ import { NavigationContainer, DefaultTheme,  } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
 import { useTheme,Box } from "native-base";
 import { AppRoutes } from "./app.routes";
-import { useContext } from "react";
 import { useAuth } from "@hooks/useAuth";
+import { Loading } from "@components/Loading";
 
 
 export function Routes(){
   const {colors}= useTheme();
-const {user} = useAuth();
+const {user, isLoadingUserStorageData} = useAuth();
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700]
 
-
+if(isLoadingUserStorageData){
+  return <Loading/>
+}
 
   return(
     <Box flex={1} bg="gray.700">
     <NavigationContainer>
-      {/* <AuthRoutes/> */}
-      <AppRoutes/>
+      {user.id ? <AppRoutes /> : <AuthRoutes/> }
+      
     </NavigationContainer>
     </Box>
   )
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -13,6 +13,7 @@ import { api } from '@services/api';
 import axios from 'axios';
 import { Alert } from 'react-native';
 import { AppError } from '@utils/AppError';
+import { useAuth } from '@hooks/useAuth';
 
 type FormDataProps ={
   name:string;
@@ -32,7 +33,10 @@ export function SignUp() {
   const {control,handleSubmit, formState:{errors}} = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema)
   });
+
+  const [isLoading, setIsLoading] = useState(false)
   const navigation = useNavigation();
+  const {signIn} = useAuth();
 
   const toast = useToast();
 
@@ -40,9 +44,13 @@ export function SignUp() {
  async function handleSignUp({email,name,password}: FormDataProps){
     
   try{
-    const response = await api.post('/users',{name,email,password})
+    setIsLoading(true)
+    await api.post('/users',{name,email,password})
+    await signIn(email,password)
 
   }catch(error){
+    setIsLoading(false);
+    
     if(axios.isAxiosError(error)){
       const isAppError =error instanceof AppError;
       const title = isAppError? error.message : 'Não foi possivel criar a conta. Tente novamente mais tarde.'
@@ -135,7 +143,11 @@ export function SignUp() {
       
    
    
-    <Button title='Criar e acessar' onPress={handleSubmit(handleSignUp)}/>
+    <Button 
+    title='Criar e acessar' 
+    onPress={handleSubmit(handleSignUp)}
+    isLoading={isLoading}
+    />
 
      </Center>
 
