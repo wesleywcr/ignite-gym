41b4be6681ad9f25364c7f1f54d9288c5e77f529
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-06-20 22:54:24 -0300

    feat: add info user in component home header

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,23 +2,27 @@ import { HStack, Heading, Icon, Text, VStack } from "native-base";
 import  {MaterialIcons } from '@expo/vector-icons'
 import { UserPhoto } from "./UserPhoto";
 import { TouchableOpacity } from "react-native";
+import { useAuth } from "@hooks/useAuth";
+
+import defautlUserPhotoImg from '@assets/userPhotoDefault.png'
 
 export function HomeHeader(){
+  const {user, signOut} = useAuth();
   return(
     <HStack bg={'gray.600'} pt={16} pb={5} px={8} alignItems={'center'}>
       <UserPhoto
-      source={{uri:'https://github.com/wesleywcr.png'}}
+      source={user.avatar? { uri:user.avatar} : defautlUserPhotoImg}
       alt="Image do usuário"
       size={16}
       mr={4}
       />
       <VStack flex={1} >
-      <Text color={'gray.100'} fontSize={'md'}>Ola</Text>
+      <Text color={'gray.100'} fontSize={'md'}>Olá</Text>
       <Heading color={'gray.100'} fontSize={'md'} fontFamily={'heading'}>
-        Wesley
+       {user.name}
       </Heading>
       </VStack>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={signOut}>
       <Icon as={MaterialIcons} name="logout" color={'gray.200'} size={7}/>
       </TouchableOpacity>
 
