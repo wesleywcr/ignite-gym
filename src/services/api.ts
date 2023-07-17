1ef0596767ce7cc9925d3bc176ce87988bb0691b
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-07-16 22:33:13 -0300

    fix: style format

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: 'http://192.168.1.11:3333'
 })
+
 api.interceptors.response.use(response => response, error => {
   if (error.response && error.response.data) {
     return Promise.reject(new AppError(error.response.data.message))
